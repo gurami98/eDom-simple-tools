9e9a9ab710efd6edae76498ce5959142b3af06dd
gurami98 <chilindrishviligurami@gmail.com>
2025-07-01 01:58:59 +0400

    add weekly stats component

---
+++ src/app/weekly-stats/weekly-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-stats',
+  templateUrl: './weekly-stats.component.html',
+  styleUrls: ['./weekly-stats.component.css']
+})
+export class WeeklyStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
